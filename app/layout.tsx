dd129bb35bbfeaf22d00dac36bcad4a09ec2bacf
Nazrin Chobanzada <nazrinchobanzada@Nazrins-MacBook-Pro.local>
2026-07-23 10:00:54 +0400

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const ibm = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -21,7 +22,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={ibm.variable}>
-      <body>{children}</body>
+      <body>{children}
+      <Analytics />
+      </body>
     </html>
   );
 }
